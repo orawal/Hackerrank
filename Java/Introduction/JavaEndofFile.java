8f41430f7ae2969c3816e562d806fa13ba85740e
Oshin Rawal <oshin.rawal94@gmail.com>
2016-06-22 19:38:05 -0400

    Create JavaEndofFile.java

---
+++ Java/Introduction/JavaEndofFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 1;
+    Scanner s = new Scanner(System.in);   
+    while (s.hasNext()) {
+        String b = s.nextLine(); 
+        System.out.println(i++ + " " + b); 
+      }
+    }
+}
